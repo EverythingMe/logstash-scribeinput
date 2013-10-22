b21158e5646adb7e964e2380edf598a7ca3d16f3
Lital Natan <litaln@everything.me>
2013-10-22 13:16:28 +0200

    Some cleanup of unneeded code

--- logstash/inputs/scribe.rb
+++ logstash/inputs/scribe.rb
@@ -12,12 +12,6 @@ java_import 'scribe_server.Util'
 
 class LogStash::Inputs::Scribe < LogStash::Inputs::Base
   class ScribeHandler < ActionScribeHandler
-    #include Scribe::Iface
-
-    #def initialize(output_queue)
-      #@output_queue = output_queue
-    #end
-
     def setQueue(output_queue)
       @output_queue = output_queue
     end
@@ -27,6 +21,7 @@ class LogStash::Inputs::Scribe < LogStash::Inputs::Base
         @output_queue << event
     end
 
+    # Example for implementing "Log" directly
     #java_signature 'ResultCode Log(List<LogEntry> messages)'
    # def Log(messages)
     #  messages.each do |message|
@@ -42,27 +37,23 @@ class LogStash::Inputs::Scribe < LogStash::Inputs::Base
   config_name "scribe"
   milestone 1
 
-  #default :codec, "plain"
   config :host, :validate => :string, :default => "0.0.0.0"
   config :port, :validate => :number, :required => true
-  #config :charset, :validate => ::Encoding.name_list, :default => "UTF-16"
 
   def initialize(*args)
     super(*args)
-  end # def initialize
+  end
 
   public
   def register
     @logger.info("Starting scribe input listener", :address => "#{@host}:#{@port}")
-  end # def register
+  end
 
   public
   def run(output_queue)
-    #handler = ScribeHandler.new(output_queue);
     handler = ScribeHandler.new()
     handler.setQueue(output_queue)
     @logger.info("Start of run for thrift plugin")
-    #Util.serveWithHandler(@host, @port, StdoutScribeHandler.new())
     Util.serveWithHandler(@host, @port, handler)
   end
 end 
